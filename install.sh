1f439e98b619ad0f6ea3d0a181897bc9544bf973
yagihashoo <yagihashoooooo@gmail.com>
2017-07-22 10:40:59 +0900

    NeoBundleをcloneするタイミングを変更

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 git clone https://github.com/yagihashoo/dotfiles.git ~/dotfiles
 
 if [ $? = 0 ]; then
+  git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
   for f in `find ~/dotfiles -name ".*" -maxdepth 1 | grep -v git`
   do
     ln -sfvn $f `basename ~/$f`
   done
-  git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
 else
   echo リポジトリのクローンに失敗しました。
 fi
