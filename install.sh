f6d2427ff1e3c100a02631ea323b357f4bc9e6c7
yagihashoo <yagihashoooooo@gmail.com>
2017-07-22 10:47:06 +0900

    -maxdepthは-nameの前がいいんだそう

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/yagihashoo/dotfiles.git ~/dotfiles
 
 if [ $? = 0 ]; then
   git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
-  for f in `find ~/dotfiles -name ".*" -maxdepth 1 | grep -v git`
+  for f in `find ~/dotfiles -maxdepth 1 -name ".*" | grep -v git`
   do
     ln -sfvn $f `basename ~/$f`
   done
