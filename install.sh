afa388e7236bff3f4d4c83af64f25c12d856a621
yagihashoo <yagihashoooooo@gmail.com>
2018-01-18 15:11:51 +0900

    gitの設定ファイルもここで管理することにした

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/yagihashoo/dotfiles.git ~/dotfiles
 
 if [ $? = 0 ]; then
   git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
-  for f in `find ~/dotfiles -maxdepth 1 -name ".*" | grep -v git`
+  for f in `find ~/dotfiles -maxdepth 1 -name ".*" | grep -v '.git$' | grep -v '.gitignore$'`
   do
     ln -sfvn $f `basename ~/$f`
   done
