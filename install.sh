c5f4f2d35d1a192cb0c976d215dacbbe7ccd4d04
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-05-28 23:46:49 +0900

    Removed neobundle

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/yagihashoo/dotfiles.git ~/dotfiles
 
 if [ $? = 0 ]; then
-  git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
   for f in `find ~/dotfiles -maxdepth 1 -name ".*" | grep -v '.git$' | grep -v '.gitignore$'`
   do
     ln -sfvn $f ~/`basename $f`
